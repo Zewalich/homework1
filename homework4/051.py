b277a410497bf0baffdb0de063c82768ca417811
Zewalich <112897563+Zewalich@users.noreply.github.com>
2022-09-20 23:58:29 +0300

    Add files via upload

---
+++ homework4/051.py
@@ -0,0 +1,17 @@
+score = int(input('Введите количество бутылок ')) #вводим количество бутылок
+print('There are', score, 'green bottles hanging on the wall.', score,
+             'green bottles hanging on the wall,  and if 1 green bottle should accidentally fall.') #
+print('how many green bottles will be hanging on the wall?') #
+answer = int(input('Введите ответ ')) #
+while answer != score - 1: #
+    if answer != score - 1:  #
+        print('no, try again')  #
+        answer = int(input('Попробуйте еще раз ')) #
+while answer == score - 1 !=0:  #
+    if answer == score -1 !=0:  #
+        print('There will be', answer, 'green bottles hanging on the wall') #
+    break
+while answer == 0: #
+    if answer == 0: #
+        print('there are no more green hanging on the wall') #
+    break
